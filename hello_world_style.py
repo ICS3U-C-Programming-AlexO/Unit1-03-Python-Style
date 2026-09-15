4018fb0094454a5d4b1840375a345fcdae69423c
Alexander O'Brien <amelia.obrien@stu.ocsb.ca>
2026-09-15 20:22:42 +0000

    First commit of hewllo_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Created By: Alex OBrien
+# Date: Sep 15th 2026
+# This program displays a reading and contains proper formatting
+def main():
+    print("Hello, Arianna!")
+    print("You're helping me learn Python!")
+
+
+if __name__ == "__main__":
+    main()
